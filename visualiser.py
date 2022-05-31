d3b733907ba2ab6c41433e482524ed2d7e5f7eba
egillthor00 olgeirs <egill1807@gmail.com>
2022-05-31 23:48:36 +0000

    working on the vizualiser

--- visualiser.py
+++ visualiser.py
@@ -1,4 +1,4 @@
-import matplotlib as matplot
+import matplotlib.pyplot as plt
 import visualiserfunctions
 
 pricing_file = "csv_files/pricing.csv"
@@ -7,3 +7,11 @@ data = visualiserfunctions.get_data_from_pricing_file(pricing_file)
 data.pop(0) # removing header
 
 answer = input("What plushie would you want to look at.\n1.Camel\n2.Chamois\n3.Jaguar\n4.Kitten\n5.Lion\n6.Monkey\n7.Nessie\n8.Panda\n9.Red_Fox\n10.Sheep\n11.Stingray\n12.Teddy_bear\n13.Wolverine\n14.Sum\nAwnser : ")
+
+X, Y = visualiserfunctions.get_query_plushie(data, int(answer))
+
+print(X)
+print(Y)
+
+plt.plot(X,Y)
+plt.show()
--- visualiserfunctions.py
+++ visualiserfunctions.py
@@ -1,5 +1,5 @@
 import csv
-import matplotlib as matplot
+import matplotlib.pyplot as plt
 
 def get_data_from_pricing_file(pricing_file):
     return_list = []
@@ -9,4 +9,17 @@ def get_data_from_pricing_file(pricing_file):
     for row in csvreader:
         return_list.append(row)
     return return_list
+
+def get_query_plushie(data, answer):
+    X_list = []
+    Y_list = []
+    for i in data:
+        if len(Y_list) == 0:
+            X_list.append(i[0])
+            Y_list.append(i[answer+1])
+        if i[answer+1] != Y_list[-1]:
+            X_list.append(i[0])
+            Y_list.append(i[answer+1])
+        if len(Y_list) == 100:
+            return X_list, Y_list
 
